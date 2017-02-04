57fa8bd7177183039b3a3782534b195fea5e20fa
fanhehe <13889441200@163.com>
2017-02-04 12:24:56 +0800

    add a new table model and modify tbl_user

---
+++ src/db/mysql/models/main/tbl_article.ts
@@ -0,0 +1,35 @@
+module.exports = function (sequelize, DataTypes) {
+	return sequelize.define('tbl_user', {
+		// id: {
+		// 	type: DataTypes.INTEGER(11),
+		// 	autoIncrement: true,
+		// },
+		author: {
+			type : DataTypes.STRING(40),
+		},
+		title: {
+			type: DataTypes.STRING(40),
+		},
+		content: {
+			type: DataTypes.TEXT,
+		},
+		create_time: {
+			type: DataTypes.DATE,
+		},
+		last_modify_time: {
+			type: DataTypes.DATE,
+		},
+		status: {
+			type: DataTypes.INTEGER(1),
+			allowNull: true,
+			defaultValue: 1,
+		},
+	}, {
+		timestamps: true,
+		primaryKey: ['author', 'title'],
+		createAt: 'create_time',
+		UpdateAt: 'last_modify_time',
+		tableName: 'tbl_article',
+		freezeTableName: true
+	});
+};
--- src/db/mysql/models/main/tbl_user.ts
+++ src/db/mysql/models/main/tbl_user.ts
@@ -1,63 +1,53 @@
 module.exports = function (sequelize, DataTypes) {
-  return sequelize.define('tbl_user', {
-	id: {
-	  type: DataTypes.INTEGER(11),
-	  autoIncrement: true,
-	  primaryKey: true,
-	},
-	username: {
-	  type : DataTypes.CHAR(100),
-	},
-	password: {
-	  type : DataTypes.CHAR(32),
-	},
-	real_name: {
-	  type : DataTypes.CHAR(50),
-	},
-	preview: {
-	  type : DataTypes.CHAR(100),
-	},
-	nickname: {
-	  type : DataTypes.CHAR(100),
-	},
-	create_time: {
-	  type: DataTypes.INTEGER(11),
-	  defaultValue:0
-	},
-	last_login_time: {
-	  type : DataTypes.INTEGER(11),
-	},
-	qq:{
-	  type: DataTypes.CHAR(20),
-	},
-	status:{
-	  type : DataTypes.INTEGER(4),
-	  defaultValue:0
-	},
-	college:{
-	  type : DataTypes.CHAR(4)
-	},
-	gender:{
-	  type : DataTypes.CHAR(2)
-	},
-	telephone:{
-	  type:DataTypes.CHAR(20)
-	},
-	credit:{
-	  type:DataTypes.INTEGER(11)
-	},
-	age:{
-	  type:DataTypes.INTEGER(4)
-	},
-	email:{
-	  type:DataTypes.CHAR(255)
-	},
-	ip:{
-	  type:DataTypes.STRING(20)
-	},
-  }, {
-	timestamps: false,
-	tableName: 'tbl_user',
-	freezeTableName: true
-  });
+	return sequelize.define('tbl_user', {
+		id: {
+			type: DataTypes.INTEGER(11),
+			autoIncrement: true,
+		},
+		username: {
+			type : DataTypes.STRING(40),
+			primaryKey: true,
+		},
+		password: {
+			type : DataTypes.STRING(40),
+		},
+		real_name: {
+			type : DataTypes.STRING(40),
+		},
+		preview: {
+			type : DataTypes.STRING(100),
+		},
+		nickname: {
+			type : DataTypes.STRING(40),
+		},
+		create_time: {
+			type: DataTypes.DATE,
+		},
+		last_login_time: {
+			type : DataTypes.DATE,
+		},
+		qq:{
+			type: DataTypes.STRING(20),
+		},
+		status:{
+			type : DataTypes.INTEGER(1),
+			defaultValue:0
+		},
+		gender:{
+			type : DataTypes.ENUM('male', 'female'),
+		},
+		telephone:{
+			type:DataTypes.CHAR(20)
+		},
+		email:{
+			type:DataTypes.STRING(40)
+		},
+		ip:{
+			type:DataTypes.STRING(20)
+		},
+	}, {
+		timestamps: false,
+		tableName: 'tbl_user',
+		freezeTableName: true
+	});
 };
